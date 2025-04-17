9c02bf2f61758023e8b249c255d6c081f259e91a
BrendanSCampbell <brendan.campbell1134@gmail.com>
2025-04-17 18:44:10 -0400

    Update sequelize.js

--- sequelize.js
+++ sequelize.js
@@ -1,6 +1,7 @@
+// sequelize.js
 const { Sequelize } = require('sequelize');
 
-// Use the DATABASE_URL from Heroku's environment
+// Get the connection string from the environment variable
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
