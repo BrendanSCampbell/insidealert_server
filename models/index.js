b8e062c76dbcb69b80312aa52e4637f78746a78a
BrendanSCampbell <brendan.campbell1134@gmail.com>
2025-04-17 16:36:20 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,24 +1,21 @@
-// User model example
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    discord_id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    stripe_customer_id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    subscription_id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    subscription_status: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+const { Sequelize, DataTypes } = require('sequelize');
 
-  return User;
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Heroku's Postgres uses self-signed certs
+    },
+  },
+});
+
+const User = require('./user')(sequelize, DataTypes);
+
+sequelize.sync(); // Or sync({ alter: true }) to update tables safely
+
+module.exports = {
+  sequelize,
+  User,
 };
