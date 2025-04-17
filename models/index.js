c2d78bcdaf945b6b0637a8da95b619bfb15d435f
BrendanSCampbell <brendan.campbell1134@gmail.com>
2025-04-17 16:26:58 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,26 +1,24 @@
-const { Sequelize, DataTypes } = require('sequelize');
+// User model example
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    discord_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    stripe_customer_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subscription_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subscription_status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
 
-// Initialize Sequelize with SQLite
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite',  // Path to SQLite database file
-});
-
-// Define User model
-const User = sequelize.define('User', {
-  discord_id: { 
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  stripe_customer_id: DataTypes.STRING,
-  subscription_id: DataTypes.STRING,
-  subscription_status: DataTypes.STRING,
-});
-
-// Sync the model with the database
-sequelize.sync({ alter: true }) // This will auto-create tables or update schema
-  .then(() => console.log('SQLite database synced'))
-  .catch((err) => console.error('SQLite sync error:', err));
-
-module.exports = { sequelize, User };
+  return User;
+};
