f42d4790d9b6c67d2f9a9053a9e6511ce8e2316a
BrendanSCampbell <brendan.campbell1134@gmail.com>
2025-04-20 07:52:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const { sequelize, User } = require('./models'); // Import sequelize and User model
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 sequelize.sync()
   .then(() => {
